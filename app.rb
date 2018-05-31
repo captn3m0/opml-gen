c5fe900f96fcb6fac170c3195dbe69116e8a77da
Nemo <me@captnemo.in>
2018-06-01 00:56:39 +0530

    Initial commit

---
+++ app.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+require 'dotenv/load'
+require 'sinatra/base'
+require 'sinatra/reloader'
+require 'octokit'
+require 'redis'
+require 'json'
+require './opml'
+
+Octokit.auto_paginate = true
+
+class MyApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    also_reload 'opml.rb'
+  end
+  configure do
+    set :r, Redis.new
+    set :client, Octokit::Client.new(
+      client_id: ENV.fetch('GITHUB_CLIENT_ID'),
+      client_secret: ENV.fetch('GITHUB_CLIENT_SECRET'),
+      per_page: 100,
+      auto_traversal: true,
+      auto_paginate: true
+    )
+  end
+
+  def get_starred_repos_from_cache(user)
+    from_cache = settings.r.get "#{user}.repos"
+    if from_cache
+      time = settings.r.get "#{user}.repos.time" || Time.now.to_i
+      return [JSON.parse(from_cache), time]
+    end
+    repos = []
+    starred = settings.client.starred(user)
+
+    starred.each do |repo|
+      repos.push repo[:full_name]
+    end
+
+    time = Time.now.to_i
+    settings.r.set "#{user}.repos", repos
+    settings.r.set "#{user}.repos.time", time
+    [repos, time]
+  end
+
+  get '/github/:user/starred.opml' do
+    user = params[:user]
+    repos, time = get_starred_repos_from_cache(user)
+    opml = OPML.new do
+      @title = "Releases: #{user}/starred"
+      @date_created = Time.new(time.to_i).rfc822
+      @date_modified = Time.now.rfc822
+      @owner_name = user
+    end
+    repos.each do |r|
+      title = "Release notes from #{r}"
+      html_url = "https://github.com/#{r}/releases"
+      rss_url  = "#{html_url}.atom"
+      opml.add_outline(
+        text: title, description: "#{r}/releases", html_url: html_url,
+        xml_url: rss_url, title: title,
+        type: 'rss', version: 'RSS2'
+      )
+    end
+
+    filename = "#{user}-starred-releases.opml"
+
+    response.headers['Content-Disposition'] = "attachment; filename=#{filename};"
+    response.headers['Content-Type'] = 'application/octet-stream'
+    response.headers['Content-Transfer-Encoding'] = 'binary'
+
+    opml.xml
+  end
+end
---
+++ opml.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+
+class OPML
+  PROPERTIES = %w[title date_created date_modified
+                  owner_name owner_email docs].freeze
+
+  attr_accessor :title, :date_created, :date_modified, :owner_name, :owner_email, :docs
+
+  def initialize(&block)
+    instance_eval(&block)
+    @docs = 'http://dev.opml.org/spec2.html'
+    @outlines = []
+  end
+
+  def camel_case(str)
+    str.downcase.split('_').each_with_index.map do |v, i|
+      i.zero? ? v : v.capitalize
+    end.join
+  end
+
+  def add_outline(params)
+    params = Hash[params.map { |k, v| [camel_case(k.to_s), v] }]
+    @outlines.push params
+  end
+
+  def add_note_with_params(xml, attrs)
+    attrs.each do |attr|
+      val = send(attr.to_sym)
+      key = camel_case(attr).to_sym
+      xml.send(key, val) if val
+    end
+  end
+
+  def xml
+    Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+      xml.opml(version: '2.0') do
+        xml.head do
+          add_note_with_params xml, PROPERTIES
+        end
+
+        xml.body do
+          xml.outline text: 'GitHub Releases' do
+            @outlines.each do |outline|
+              xml.outline outline
+            end
+          end
+        end
+      end
+    end.to_xml
+  end
+end
